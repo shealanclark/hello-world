1ca478dd707522e1f59af56f2367dcb950d69bac
billgx <billgx@ksu.edu>
2017-08-17 09:51:15 -0500

    Testing this out
    Uploading asset files from a Unity fish game project

---
+++ Assets/code/Fish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour {
+	
+	[SerializeField]
+	private float _upwardForceMultiplier = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		bool pressedFireButton = Input.GetButtonDown("Fire1");
+		if (pressedFireButton)
+		{
+			Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.AddForce(Vector3.up * _upwardForceMultiplier);
+		}
+	}
+}
---
+++ Assets/code/moveLeft.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveLeft : MonoBehaviour {
+
+	[SerializeField]
+	private float _speed = 5f;
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * Time.deltaTime * _speed);
+		if (transform.position.x < -15)
+		{
+			float randomYPosition = UnityEngine.Random.Range(-3, 3);
+			transform.position = new Vector3(15, randomYPosition, 0);
+			//Debug.Log (randomYPosition);
+		}
+	}
+}
